5338fd2d926045ad52faede7d2d8fb9ebc52b480
paulmandole <paulmandole@gmail.com>
2022-03-21 22:27:45 -0300

    ejercicios

---
+++ ejercicios/Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ingresar 5 números por consola, guardándolos en una variable escalar. Luego calcular y mostrar: el valor máximo, el valor mínimo y el promedio
+            int num;
+            int max = 0;
+            int flag = 0;
+            string numText;
+
+
+            for (int i = 0; i < 5; i++)
+            {
+                do
+                {
+                    Console.WriteLine("Ingrese un numero:");
+                    numText = Console.ReadLine();
+
+                }while (!int.TryParse(numText, out num));
+                if (flag == 0)
+                {
+                    max = num;
+                    flag = 1;
+                }
+                else 
+                {
+                    if(max < num)
+                    {
+                        max = num;
+                    }
+                }
+            }
+            Console.WriteLine("El numero maximo es:{0}",max);
+
+            
+        }
+    }
+}
---
+++ ejercicios/Ejercicio2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ingresar un número y mostrar el cuadrado y el cubo del mismo. Se debe validar que el número sea mayor que cero,
+            //caso contrario, mostrar el mensaje: "ERROR. ¡Reingresar número!".
+            int num;
+            int cubo;
+            int cuadrado;
+            string numText;
+
+            do 
+            {
+                Console.WriteLine("Ingrese un numero:");
+                numText = Console.ReadLine();
+
+
+            }while (int.TryParse(numText,out num) || num == 0);
+            
+
+        }
+    }
+}
